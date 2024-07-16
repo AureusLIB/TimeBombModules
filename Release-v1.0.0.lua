b5df9c3619d3e9961d905c14fb813e9044c25559
AureusLIB <arcticbhop@gmail.com>
2024-07-16 23:23:12 +0100

    Update Release-v1.0.0.lua

--- Release-v1.0.0.lua
+++ Release-v1.0.0.lua
@@ -1,26 +1,402 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua"))()
+local ThemeManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/addons/ThemeManager.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/addons/SaveManager.lua"))()
+
+local RunScript = true
+
+--Script variables
+local TrackAssist = {
+	Enabled = false,
+	Strength = 2,
+	UseGradient = false,
+	MinGradient = 0,
+	MaxGradient = 1.3,
+	GradientCentre = 3,
+	UseThreshold = false,
+	ActivateThreshold = 0.25,
+	DeactivateThreshold = 64,
+	MaxDistance = 9,
+	Overtime = 0.05
+}
+
+local HoldAssist = {
+	Enabled = false,
+	Strength = 2,
+	UseGradient = false,
+	MinGradient = 0,
+	MaxGradient = 1.3,
+	GradientCentre = 3,
+	UseThreshold = false,
+	ActivateThreshold = 0.25,
+	DeactivateThreshold = 64,
+	MaxDistance = 20,
+	Overtime = 0.05
+}
+
+local Window = Library:CreateWindow({
+	Title = 'Timebomb',
+	Center = true,
+	AutoShow = true,
+	TabPadding = 8,
+	MenuFadeTime = 0.2,
+	Overtime = 0.05
+})
+
+local Tabs = {
+	Legit = Window:AddTab('Legit'),
+	UISettings = Window:AddTab('UI Settings'),
+}
+
+--TRACKING
+
+local LegitMainLeft = Tabs.Legit:AddLeftGroupbox("Track Assist")
+
+local TrackAssistToggle = LegitMainLeft:AddToggle('TrackAssist', {
+	Text = 'Track assist',
+	Default = false,
+	Tooltip = 'Aim assist for tracking',
+
+	Callback = function(Value)
+		TrackAssist.Enabled = Value
+	end
+})
+
+local TAUseGradient = LegitMainLeft:AddToggle('TAUseGradient', {
+	Text = 'Use gradient',
+	Default = false,
+	Tooltip = 'Looks more legit (faster mouse = higher strength)',
+
+	Callback = function(Value)
+		TrackAssist.UseGradient = Value
+	end
+})
+
+local TAThreshold = LegitMainLeft:AddToggle('TAUseThreshold', {
+	Text = 'Use activate threshold',
+	Default = false,
+	Tooltip = 'stops it locking on low and high speeds',
+
+	Callback = function(Value)
+		TrackAssist.UseThreshold = Value
+	end
+})
+
+local TrackAssistKeyPicker = TrackAssistToggle:AddKeyPicker('TrackAssistKeybind', {
+	Default = 'LeftAlt',
+	SyncToggleState = false,
+	Mode = 'Hold',
+
+	Text = 'Tracking aim assist',
+	NoUI = true
+})
+
+local TAGradientBox = LegitMainLeft:AddDependencyBox()
+
+TAGradientBox:SetupDependencies({
+	{ Toggles.TAUseGradient, true }
+})
+
+TAGradientBox:AddSlider('TAMinGrad', {
+	Text = 'Minimum gradient',
+	Default = 0,
+	Min = 0,
+	Max = 3,
+	Rounding = 1,
+	Compact = false,
+
+	Callback = function(Value)
+		TrackAssist.MinGradient = Value
+	end
+})
+
+TAGradientBox:AddSlider('TAMaxGrad', {
+	Text = 'Maximum gradient',
+	Default = 1.2,
+	Min = 0,
+	Max = 3,
+	Rounding = 1,
+	Compact = false,
+
+	Callback = function(Value)
+		TrackAssist.MaxGradient = Value
+	end
+})
+
+TAGradientBox:AddSlider('TAGradCentre', {
+	Text = 'Gradient centre',
+	Default = 3,
+	Min = 0,
+	Max = 32,
+	Rounding = 1,
+	Compact = false,
+
+	Callback = function(Value)
+		TrackAssist.GradientCentre = Value
+	end
+})
+
+local TAThresholdBox = LegitMainLeft:AddDependencyBox()
+
+TAThresholdBox:SetupDependencies({
+	{ Toggles.TAUseThreshold, true }
+})
+
+TAThresholdBox:AddSlider('TAActivateThreshold', {
+	Text = 'Activate threshold',
+	Default = 0.25,
+	Min = 0,
+	Max = 5,
+	Rounding = 2,
+	Compact = false,
+
+	Callback = function(Value)
+		TrackAssist.ActivateThreshold = Value
+	end
+})
+
+TAThresholdBox:AddSlider('TADeactivateThreshold', {
+	Text = 'Deactivate threshold',
+	Default = 64,
+	Min = 0,
+	Max = 75,
+	Rounding = 0,
+	Compact = false,
+
+	Callback = function(Value)
+		TrackAssist.DeactivateThreshold = Value
+	end
+})
+
+
+LegitMainLeft:AddSlider('TrackAssistStrength', {
+	Text = 'Track assist strength',
+	Default = 2,
+	Min = 0,
+	Max = 5,
+	Rounding = 1,
+	Compact = false,
+
+	Callback = function(Value)
+		TrackAssist.Strength = Value
+	end
+})
+
+LegitMainLeft:AddSlider('TAOvertime', {
+	Text = 'Overtime',
+	Default = 0.05,
+	Min = 0,
+	Max = 0.25,
+	Rounding = 2,
+	Compact = false,
+
+	Callback = function(Value)
+		TrackAssist.Overtime = Value
+	end
+})
+
+LegitMainLeft:AddSlider('TAMaxDist', {
+	Text = 'Max distance',
+	Default = 9,
+	Min = 0,
+	Max = 16,
+	Rounding = 1,
+	Compact = false,
+
+	Callback = function(Value)
+		TrackAssist.MaxDistance = Value
+	end
+})
+
+
+
+--HOLDING
+
+local LegitMainRight = Tabs.Legit:AddRightGroupbox("Holding Assist")
+
+local HoldAssistToggle = LegitMainRight:AddToggle('HoldAssist', {
+	Text = 'Hold assist',
+	Default = false,
+	Tooltip = 'Aim assist for holding',
+
+	Callback = function(Value)
+		HoldAssist.Enabled = Value
+	end
+})
+
+local HAUseGradient = LegitMainRight:AddToggle('HAUseGradient', {
+	Text = 'Use gradient',
+	Default = false,
+	Tooltip = 'Looks more legit (faster mouse = higher strength)',
+
+	Callback = function(Value)
+		HoldAssist.UseGradient = Value
+	end
+})
+
+local HAThreshold = LegitMainRight:AddToggle('HAUseThreshold', {
+	Text = 'Use activate threshold',
+	Default = false,
+	Tooltip = 'stops it locking on low and high speeds',
+
+	Callback = function(Value)
+		HoldAssist.UseThreshold = Value
+	end
+})
+
+local HoldAssistKeyPicker = HoldAssistToggle:AddKeyPicker('HoldAssistKeybind', {
+	Default = 'Space',
+	SyncToggleState = false,
+	Mode = 'Hold',
+
+	Text = 'Holding aim assist',
+	NoUI = true
+})
+
+local HAGradientBox = LegitMainRight:AddDependencyBox()
+
+HAGradientBox:SetupDependencies({
+	{ Toggles.HAUseGradient, true }
+})
+
+HAGradientBox:AddSlider('HAMinGrad', {
+	Text = 'Minimum gradient',
+	Default = 0,
+	Min = 0,
+	Max = 3,
+	Rounding = 1,
+	Compact = false,
+
+	Callback = function(Value)
+		HoldAssist.MinGradient = Value
+	end
+})
+
+HAGradientBox:AddSlider('HAMaxGrad', {
+	Text = 'Maximum gradient',
+	Default = 1.2,
+	Min = 0,
+	Max = 3,
+	Rounding = 1,
+	Compact = false,
+
+	Callback = function(Value)
+		HoldAssist.MaxGradient = Value
+	end
+})
+
+HAGradientBox:AddSlider('HAGradCentre', {
+	Text = 'Gradient centre',
+	Default = 3,
+	Min = 0,
+	Max = 32,
+	Rounding = 1,
+	Compact = false,
+
+	Callback = function(Value)
+		HoldAssist.GradientCentre = Value
+	end
+})
+
+local HAThresholdBox = LegitMainRight:AddDependencyBox()
+
+HAThresholdBox:SetupDependencies({
+	{ Toggles.HAUseThreshold, true }
+})
+
+HAThresholdBox:AddSlider('HAActivateThreshold', {
+	Text = 'Activate threshold',
+	Default = 0.25,
+	Min = 0,
+	Max = 5,
+	Rounding = 2,
+	Compact = false,
+
+	Callback = function(Value)
+		HoldAssist.ActivateThreshold = Value
+	end
+})
+
+HAThresholdBox:AddSlider('HADeactivateThreshold', {
+	Text = 'Deactivate threshold',
+	Default = 64,
+	Min = 0,
+	Max = 75,
+	Rounding = 0,
+	Compact = false,
+
+	Callback = function(Value)
+		HoldAssist.DeactivateThreshold = Value
+	end
+})
+
+
+LegitMainRight:AddSlider('HoldAssistStrength', {
+	Text = 'Hold assist strength',
+	Default = 2,
+	Min = 0,
+	Max = 5,
+	Rounding = 1,
+	Compact = false,
+
+	Callback = function(Value)
+		HoldAssist.Strength = Value
+	end
+})
+
+LegitMainRight:AddSlider('HAOvertime', {
+	Text = 'Overtime',
+	Default = 0.05,
+	Min = 0,
+	Max = 0.25,
+	Rounding = 2,
+	Compact = false,
+
+	Callback = function(Value)
+		HoldAssist.Overtime = Value
+	end
+})
+
+LegitMainRight:AddSlider('HAMaxDist', {
+	Text = 'Max distance',
+	Default = 20,
+	Min = 0,
+	Max = 20,
+	Rounding = 1,
+	Compact = false,
+
+	Callback = function(Value)
+		HoldAssist.MaxDistance = Value
+	end
+})
+
+
+
+local MenuGroup = Tabs.UISettings:AddLeftGroupbox('Menu')
+MenuGroup:AddButton('Unload', function() RunScript = false Library:Unload() end)
+MenuGroup:AddLabel('Menu bind'):AddKeyPicker('MenuKeybind', { Default = 'End', NoUI = true, Text = 'Menu keybind' })
+Library.ToggleKeybind = Options.MenuKeybind
+ThemeManager:SetLibrary(Library)
+SaveManager:SetLibrary(Library)
+SaveManager:IgnoreThemeSettings()
+SaveManager:SetIgnoreIndexes({'MenuKeybind'})
+ThemeManager:SetFolder('Aureus')
+SaveManager:SetFolder('Aureus/Timebomb')
+SaveManager:BuildConfigSection(Tabs.UISettings)
+ThemeManager:ApplyToTab(Tabs.UISettings)
+SaveManager:LoadAutoloadConfig()
+
+--Script section
+
 local RunService = game:GetService("RunService")
-local UIS = game:GetService("UserInputService")
+local UserInputService = game:GetService("UserInputService")
 local Players = game:GetService("Players")
+
 local LocalPlayer = Players.LocalPlayer
+local Camera = workspace.CurrentCamera
+
 local MouseIsMoving = false
-local MouseIsMovingLF = false
-local LastFace = Vector3.new(0,0,0)
-local DirChange = 0
-
-local Bind = Enum.KeyCode.LeftAlt
-local Strength = 3
-
-local UseGradient = true
-local GradientCentre = 3
-local MinGradient = 0
-local MaxGradient = 1.2
-local ActivateThreshold = 0.25
-local DeactivateThreshold = 64
-local OverTime = 0.05
-local MaxDistance = 20
-
-
-
+local MouseWasMoving = false
+local LastFace = Vector3.zero
 local StoppedAt = tick()
 
 local function VectorAngle(a,b)
@@ -28,7 +404,7 @@ local function VectorAngle(a,b)
 	return math.acos(a.Unit:Dot(b.Unit))
 end
 
-local function OnChange(Input,gpe)
+local function OnInputChange(Input,gpe)
 	if Input.UserInputType == Enum.UserInputType.MouseMovement then
 		if math.abs(Input.Delta.X) > 0 then
 			MouseIsMoving = true
@@ -73,52 +449,81 @@ local function ClosestPlayer(Pos: Vector3)
 	return {Position = ClosestPosition,Player = Closest,Distance = Dist,RootPart = HumRP}
 end
 
-local function Main(step)
-	if LocalPlayer.Character then
-		local Character = LocalPlayer.Character
-		if Character:FindFirstChild("HumanoidRootPart") then
-			local HRP: BasePart = Character:FindFirstChild("HumanoidRootPart")
-			local Camera = workspace.CurrentCamera
-			if UIS:IsKeyDown(Enum.KeyCode.Space) then
-				local Target = ClosestPlayer(HRP.Position)
-				if Target.RootPart and Target.Distance <= MaxDistance then
-					local TargetPos = HRP.Position + (HRP.Position-Target.RootPart.Position)
-					local CurrentPos = HRP.Position
-					local ClampedStrength = math.min(Strength * step,1)
-					local Facing = (((HRP.CFrame * CFrame.new(Vector3.new(-1,0,-1))).Position - HRP.Position) * Vector3.new(1,0,1)).Unit
-					
-					local FaceAngle = VectorAngle(Facing,LastFace)/step
-					if FaceAngle > ActivateThreshold and FaceAngle < DeactivateThreshold then
-						ClampedStrength *= math.clamp(FaceAngle/GradientCentre,MinGradient,MaxGradient)	
-					else
-						ClampedStrength = 0
-					end
-					
-					
-					if tick()-StoppedAt < OverTime and not MouseIsMoving and OverTime ~= 0 then
-						ClampedStrength *= math.clamp((OverTime - (tick()-StoppedAt))/OverTime,0,1)
-					end
-					
-					local CamOffset = HRP.CFrame:ToObjectSpace(Camera.CFrame)
-					local FaceFrom = (HRP.CFrame * CFrame.Angles(0,-0.78539816339,0) * Vector3.new(0,0,-(TargetPos-CurrentPos).Magnitude))
-					FaceFrom = Vector3.new(FaceFrom.X,HRP.Position.Y,FaceFrom.Z)
-					local EP =  FaceFrom + ((TargetPos-FaceFrom) * ClampedStrength)
-					if MouseIsMoving or (tick()-StoppedAt < OverTime) then
-						local Dir = Face(EP,HRP,CFrame.Angles(0,0.78539816339,0))
-						if Dir then
-							Camera.CFrame = Dir * CamOffset
-						end
-					elseif MouseIsMovingLF then
-						StoppedAt = tick()
-					end
+local function AimAssist(Position: Vector3,Settings,Step)
+	local Character = LocalPlayer.Character
+	local HRP = Character:FindFirstChild("HumanoidRootPart")
+	local CurrentPos = HRP.Position
+	
+	if (CurrentPos - Position).Magnitude > Settings.MaxDistance then
+		return
+	end
+	
+	local ClampedStrength = math.min(TrackAssist.Strength * Step,1)
+	local Facing = (((HRP.CFrame * CFrame.new(Vector3.new(-1,0,-1))).Position - HRP.Position) * Vector3.new(1,0,1)).Unit
+
+	local FaceAngle = VectorAngle(Facing,LastFace)/Step
+
+	if FaceAngle > Settings.ActivateThreshold and FaceAngle < Settings.DeactivateThreshold then
+		ClampedStrength *= math.clamp(FaceAngle/Settings.GradientCentre,Settings.MinGradient,Settings.MaxGradient)	
+	else
+		ClampedStrength = 0
+	end
+
+	if tick()-StoppedAt < Settings.Overtime and not MouseIsMoving and Settings.OverTime ~= 0 then
+		ClampedStrength *= math.clamp((Settings.Overtime - (tick()-StoppedAt))/Settings.Overtime,0,1)
+	end
+
+	local CamOffset = HRP.CFrame:ToObjectSpace(Camera.CFrame)
+	local FaceFrom = (HRP.CFrame * CFrame.Angles(0,-0.78539816339,0) * Vector3.new(0,0,-(Position-CurrentPos).Magnitude))
+	FaceFrom = Vector3.new(FaceFrom.X,HRP.Position.Y,FaceFrom.Z)
+	local EP =  FaceFrom + ((Position-FaceFrom) * ClampedStrength)
+	if MouseIsMoving or (tick()-StoppedAt < Settings.Overtime) then
+		local Dir = Face(EP,HRP,CFrame.Angles(0,0.78539816339,0))
+		if Dir then
+			Camera.CFrame = Dir * CamOffset
+		end
+	elseif MouseWasMoving then
+		StoppedAt = tick()
+	end
+end
+
+local function MainScript(Step)
+	Camera = workspace.CurrentCamera
+	
+	if not RunScript then
+		return
+	end
+	
+	local Character: Model = LocalPlayer.Character
+	if Character then
+		local HRP: Part = Character:FindFirstChild("HumanoidRootPart")
+		
+		if HRP then
+			local ClosestPlayer = ClosestPlayer(HRP.Position)
+			
+			
+			if TrackAssist.Enabled then
+				local AssistTrack = Options.TrackAssistKeybind:GetState()
+				if AssistTrack then
+					AimAssist(ClosestPlayer.Position,TrackAssist,Step)
 				end
 			end
+			
+			if HoldAssist.Enabled then
+				local AssistHold = Options.HoldAssistKeybind:GetState()
+				if AssistHold then
+					local Difference = ClosestPlayer.Position - HRP.Position
+					AimAssist(HRP.Position - Difference,HoldAssist,Step)
+				end
+			end
+			
 			LastFace = (((HRP.CFrame * CFrame.new(Vector3.new(-1,0,-1))).Position - HRP.Position) * Vector3.new(1,0,1)).Unit
 		end
 	end
-	MouseIsMovingLF = MouseIsMoving
+
+	MouseWasMoving = MouseIsMoving
 	MouseIsMoving = false
 end
 
-UIS.InputChanged:Connect(OnChange)
-RunService.RenderStepped:Connect(Main)
+UserInputService.InputChanged:Connect(OnInputChange)
+RunService.RenderStepped:Connect(MainScript)
